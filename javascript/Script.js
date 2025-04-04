71a3ce581b49bd8c0e743e2d04022b9973fd62aa
Bramara Tejaswini <bramaratejaswini09@gmail.com>
2025-04-05 00:12:23 +0530

    shopping cart java script

--- javascript/Script.js
+++ javascript/Script.js
@@ -1,4 +1,9 @@
-let searchform=document.querySelector('.search-form');
-document.querySelector('#search-btn').onclick=()=>{
-    searchform.classList.toggle('active');
-}
+let searchform = document.querySelector(".search-form");
+document.querySelector("#search-btn").onclick = () => {
+  searchform.classList.toggle("active");
+};
+
+let shoppingcart = document.querySelector(".shopping-cart");
+document.querySelector("#cart-btn").onclick = () => {
+  shoppingcart.classList.toggle("active");
+};
