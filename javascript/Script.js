d516bc1df6e5747f13bacc7a6fc2a0d86fd00028
Bramara Tejaswini <bramaratejaswini09@gmail.com>
2025-04-05 01:04:10 +0530

    contact html,css,javaScript done

--- javascript/Script.js
+++ javascript/Script.js
@@ -7,3 +7,8 @@ let shoppingcart = document.querySelector(".shopping-cart");
 document.querySelector("#cart-btn").onclick = () => {
   shoppingcart.classList.toggle("active");
 };
+
+let loginform = document.querySelector(".login-form");
+document.querySelector("#contact-btn").onclick = () => {
+  loginform.classList.toggle("active");
+};
